c8e0f762f2d2e6899ceb168b165265fa9860f88a
batmanShu <shengjieli897@163.com>
2020-08-14 10:00:07 +0800

    Update UniquePtr.cpp

--- 智能指针/UniquePtr.cpp
+++ 智能指针/UniquePtr.cpp
@@ -7,9 +7,27 @@ public:
         ptr=raw_ptr;
     }
 
+    UniquePtr(UniquePtr&& p){
+        if(ptr!=p.ptr)
+        {
+            ptr=p.ptr;
+            p.ptr=nullptr;
+        }
+        return *this;
+    }
+
     UniquePtr(const UniquePtr& p)=delete;
 
     UniquePtr& operator = (const UniquePtr& p)=delete;
+
+    UniquePtr& operator = (UniquePtr&& p){
+        if(ptr!=p.ptr)
+        {
+            ptr=p.ptr;
+            p.ptr=nullptr;
+        }
+        return *this;
+    }
 
     T& operator * () {
         return *ptr;
