ef7015a2e40c2c2bf3ee96dbaf8c1b1e2dac0821
batmanShu <shengjieli897@163.com>
2020-10-10 15:53:18 +0800

    增加rehash函数和一些特殊情况

--- 数据结构/hashmap.cpp
+++ 数据结构/hashmap.cpp
@@ -11,38 +11,60 @@ class hashmap{
         };
         int bucket_size;
         int _size;
+        int max_size;
         vector<Node*> container;
+        T2 val_null;
+
     public:
-        hashmap(int n):bucket_size(n){
+        hashmap(int n,int N):bucket_size(n),max_size(N){
             container.resize(n);
             _size=0;
         }
 
         void insert(T1 key,T2 val){
             int hashval=hash(key);
-            Node* node=new Node(key,val);
-            node->next=container[hashval];
-            container[hashval]=node;
-            _size++;
+            Node* curnode=container[hashval];
+            while(curnode&&curnode->key!=key)
+            {
+                curnode=curnode->next;
+            }
+            if(!curnode)
+            {
+                Node* node=new Node(key,val);
+                node->next=container[hashval];
+                container[hashval]=node;
+                _size++;
+                if(_size>max_size*0.75)
+                    rehash();
+            }
+            else
+            {
+                curnode->val=val;
+            }
+            
         }
 
         T2& find(const T1& key){
             int hashval=hash(key);
             Node* curnode=container[hashval];
-            while(curnode->key!=key)
+            while(curnode&&curnode->key!=key)
             {
                 curnode=curnode->next;
             }
+            if(!curnode)
+                return val_null;
             return curnode->val;
         }
 
         T2& operator[](const T1& key){
             int hashval=hash(key);
             Node* curnode=container[hashval];
-            while(curnode->key!=key)
+            while(curnode&&curnode->key!=key)
             {
                 curnode=curnode->next;
             }
+            if(!curnode)
+                return val_null;
             return curnode->val;
         }
 
@@ -50,11 +72,15 @@ class hashmap{
             int hashval=hash(key);
             Node* prevnode=nullptr;
             Node* curnode=container[hashval];
-            while(curnode->key!=key)
+            while(curnode&&curnode->key!=key)
             {
                 prevnode=curnode;
                 curnode=curnode->next;
             }
+            //没找到对应的key，直接退出
+            if(!curnode)
+                return;
+            //找到了，进入删除操作
             if(!prevnode)
             {
                 container[hashval]=curnode->next;
@@ -77,4 +103,22 @@ class hashmap{
             return _size;
         }
 
+        void rehash(){
+            vector<Node*> Oldcontainer=container;
+            container.resize(2*bucket_size);
+            bucket_size=2*bucket_size;
+            max_size=2*max_size;
+            for(int i=0;i<container.size();i++)
+                container[i]=nullptr;
+            _size=0;
+            for(int i=0;i<Oldcontainer.size();i++)
+            {
+                Node* curnode=Oldcontainer[i];
+                while(curnode)
+                {
+                    insert(curnode->key,curnode->val);
+                    curnode=curnode->next;
+                }
+            }
+        }
 };
